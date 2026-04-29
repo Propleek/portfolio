e308f30d4c2448de8bdd8ba09c4843cd6402d9b4
Prop <51509630+Propleek@users.noreply.github.com>
2026-04-29 12:01:56 +0200

    upadte compétences descriptions

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -15,6 +15,22 @@ export class LandingPageComponent {
 
   emailRevealed = false;
 
+  techCompetences = [
+    { label: 'Angular', icon: 'web', route: '/competences/angular', level: 'Confirmé' },
+    { label: 'Java / SpringBoot', icon: 'code', route: '/competences/java-springboot', level: 'Confirmé' },
+    { label: 'API', icon: 'api', route: '/competences/api', level: 'Confirmé' },
+    { label: 'Docker', icon: 'storage', route: '/competences/docker', level: 'Avancé' },
+    { label: 'React', icon: 'widgets', route: '/competences/react', level: 'Intermédiaire' },
+    { label: 'Jenkins CI/CD', icon: 'autorenew', route: '/competences/jenkins', level: 'Avancé' },
+  ];
+
+  humanCompetences = [
+    { label: 'Collaboration', icon: 'group', route: '/competences/collaboration', level: 'Confirmé' },
+    { label: 'Communication écrite', icon: 'description', route: '/competences/communication', level: 'Confirmé' },
+    { label: 'Esprit critique', icon: 'psychology', route: '/competences/critique', level: 'Confirmé' },
+    { label: 'Jugement et prise de décision', icon: 'balance', route: '/competences/jugement', level: 'Confirmé' },
+  ];
+
   public scrollTo(elementId: string): void {
     this.viewportScroller.scrollToAnchor(elementId);
   }
